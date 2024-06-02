fd1870775b136205595d883bfe58450e17b71c2e
ryfiii <mzk114.cube@gmail.com>
2024-06-02 14:52:49 +0900

    必要なマイグレーションファイル作成

---
+++ database/migrations/2024_06_02_141912_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->index(); // 公開URL用のスラッグ
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('description'); // 説明
+            $table->text('code'); // コード
+            $table->integer('likes')->default(0); // いいね数
+            $table->unsignedTinyInteger('publish_status')->default(0); // 公開ステータス(0: 非公開, 1: 公開, 2: 限定公開
+            $table->boolean('is_draft')->default(true); // 下書きフラグ
+            $table->timestamp('published_at')->nullable(); // 公開日時
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
